a245bd4a378b163c663d4aad6b96085b5e04b4c3
Shivanjali More <shivanjalimore035@gmail.com>
2023-12-12 19:40:41 +0530

    Create Products.java

---
+++ src/com/Beans/Products.java
@@ -0,0 +1,50 @@
+package com.Beans;
+
+public class Products
+{
+  private int  productID;
+  private String  Name;
+  private String  Type;
+  private int Quantity;
+  private float  Price;
+  
+  private String s;
+  
+public int getProductID() {
+	return productID;
+}
+public void setProductID(int productID) {
+	this.productID = productID;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public String getType() {
+	return Type;
+}
+public void setType(String type) {
+	Type = type;
+}
+public int getQuantity() {
+	return Quantity;
+}
+public void setQuantity(int quantity) {
+	Quantity = quantity;
+}
+public float getPrice() {
+	return Price;
+}
+public void setPrice(float price) {
+	Price = price;
+}
+public String getS() {
+	return s;
+}
+public void setS(String s) {
+	this.s = s;
+}
+
+}
