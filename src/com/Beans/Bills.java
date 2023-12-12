c58a7e68e204407f94fc89c487ac5d002acadaa2
Shivanjali More <shivanjalimore035@gmail.com>
2023-12-12 19:40:57 +0530

    Create Bills.java

---
+++ src/com/Beans/Bills.java
@@ -0,0 +1,28 @@
+package com.Beans;
+
+public class Bills
+{
+  private int  BillID;
+  private String Name;
+  private Float Total_Amount;
+  
+public int getBillID() {
+	return BillID;
+}
+public void setBillID(int billID) {
+	BillID = billID;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public Float getTotal_Amount() {
+	return Total_Amount;
+}
+public void setTotal_Amount(Float total_Amount) {
+	Total_Amount = total_Amount;
+}
+
+}
