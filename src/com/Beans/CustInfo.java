9b074f00f97824f76cabb57804679e69ea92a6aa
Shivanjali More <shivanjalimore035@gmail.com>
2023-12-12 19:40:48 +0530

    Create CustInfo.java

---
+++ src/com/Beans/CustInfo.java
@@ -0,0 +1,55 @@
+package com.Beans;
+
+public class CustInfo {
+	
+	private int CustId;
+	private String Password;
+	private String Name;
+	private int Age;
+	private String Email;
+	private String Address;
+	private String ContactNumber;
+	public int getCustId() {
+		return CustId;
+	}
+	public void setCustId(int custId) {
+		CustId = custId;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Name = password;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getContactNumber() {
+		return ContactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		ContactNumber = contactNumber;
+	}
+
+}
