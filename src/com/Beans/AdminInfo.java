547988266253779bc4cdff9c58749b5d782b0159
Shivanjali More <shivanjalimore035@gmail.com>
2023-12-12 19:41:02 +0530

    Create AdminInfo.java

---
+++ src/com/Beans/AdminInfo.java
@@ -0,0 +1,72 @@
+package com.Beans;
+
+public class AdminInfo {
+	
+	private int AdminId;
+	private String Password;
+	private String Name;
+	private int Age;
+	private String Email;
+	private String Address;
+	private String ContactNumber;
+	
+	 private int fc;  
+	    private String s;
+
+	
+	public int getAdminId() {
+		return AdminId;
+	}
+	public void setAdminId(int adminId) {
+		AdminId = adminId;
+	}
+	public String getPassword() {
+		return Name;
+	}
+	public void setPassword(String password) {
+		Name = password;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getContactNumber() {
+		return ContactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		ContactNumber = contactNumber;
+	}
+	public int getFc() {
+		return fc;
+	}
+	public void setFc(int fc) {
+		this.fc = fc;
+	}
+	public String getS() {
+		return s;
+	}
+	public void setS(String s) {
+		this.s = s;
+	}
+
+}
